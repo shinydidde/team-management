49a27ba6106238eb51273c49160e143db4bfb1a0
Mrudula Didde <mruduladidde@Mrudulas-MacBook-Pro.local>
2020-05-14 01:39:11 +0530

    Updates

---
+++ precache-manifest.22e0b4c12d85546f4988fa4c6a1dec81.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09ec450592375ae07caff470404a0d32",
+    "url": "/team-management/index.html"
+  },
+  {
+    "revision": "4710c7a467e7518b5274",
+    "url": "/team-management/static/css/main.76ff40f4.chunk.css"
+  },
+  {
+    "revision": "385a1edc033c80886bf0",
+    "url": "/team-management/static/js/2.492e8b6c.chunk.js"
+  },
+  {
+    "revision": "4710c7a467e7518b5274",
+    "url": "/team-management/static/js/main.9c8cd850.chunk.js"
+  },
+  {
+    "revision": "0b27ff1b7dfefc88512d",
+    "url": "/team-management/static/js/runtime-main.b5ba119f.js"
+  }
+]);
