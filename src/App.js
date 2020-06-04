90cf012f5fabd3f9a218861cbfdd67a117e38b89
Mrudula Didde <mruduladidde@Mrudulas-MacBook-Pro.local>
2020-06-05 01:14:44 +0530

    prevent ctrl shift c

--- src/App.js
+++ src/App.js
@@ -13,8 +13,10 @@ class App extends Component {
       $("body").keydown(function(event) {
           if (event.keyCode === 123) { // Prevent F12
             return false;
-          } else if (event.ctrlKey && event.shiftKey && event.keyCode === 73) { // Prevent Ctrl+Shift+I        
+          } else if (event.ctrlKey && event.shiftKey && event.keyCode === 73 ) { // Prevent Ctrl+Shift+I        
               return false;
+          } else if (event.shiftKey || event.ctrlKey || event.ctrlKey && event.shiftKey && event.keyCode === 67){
+            return false;
           }
         });
       });
